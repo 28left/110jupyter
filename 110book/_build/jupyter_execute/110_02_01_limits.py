4c5496d8bd2a4f9c02b95b6d0c86e643517822c6
Jan Reimann <jan.reimann@psu.edu>
2021-05-24 16:43:02 -0400

    110 PSL book initial commit

---
+++ 110book/_build/jupyter_execute/110_02_01_limits.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Limit of a Function
+
+# ## Definition
+# 
+# The function $f(x)$ has a limit of L as $x$ approaches $a$,
+# 
+# $$ \lim_{x\to a} f(x) = L$$
+# 
+# if the value of $f(x)$ can be made as close to the number $L$ as we please by taking x values *sufficiently close* to, but not equal to, $a$.
+
+# This is a text 
+# 
+# 
+# ```{math}
+#    \begin{eqnarray}
+#       y    & = & ax^2 + bx + c \\
+#       f(x) & = & x^2 + 2xy + y^2
+#    \end{eqnarray}
+# ```
+
+# In[1]:
+
+
+1+1
+
+
+# In[ ]:
+
+
+
+
