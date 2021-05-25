f6f91ae436cdc160528426458d44e88fbbb82e63
Jan Reimann <jan.reimann@psu.edu>
2021-05-24 23:04:37 -0400

    22 activity with widgets

---
+++ _build/jupyter_execute/22_activity_5_02.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+from cyllene import *
+get_ipython().run_line_magic('run', '22_activity_5_02.py')
+
+
+# # Activity 5 -- Exponential and Logarithmic Functions
+
+# ## Part 2 - Relating the exponential function to its corresponding inverse function
+# 
+# Utilize the ideas from Part 1 to explore the function $f(x) = 4^x$
+
+# In[2]:
+
+
+f= function('4^x')
+
+
+# In[3]:
+
+
+graph(f(x))
+
+
+# ## Exercise -- Properties of $f(x) = 4^x$
+# 
+# [*For each of the problems below, enter your answer in the answer cell and execute it (`Shift+Enter`)*]
+
+# In[4]:
+
+
+get_ipython().run_line_magic('problem', '1')
+
+
+# In[5]:
+
+
+get_ipython().run_cell_magic('answer', 'Problem 1', 'T')
+
+
+# ---
+
+# In[6]:
+
+
+get_ipython().run_line_magic('problem', '2')
+
+
+# In[7]:
+
+
+get_ipython().run_cell_magic('answer', 'Problem 2', '')
+
+
+# ---
+
+# In[8]:
+
+
+get_ipython().run_line_magic('problem', '3')
+
+
+# In[9]:
+
+
+get_ipython().run_cell_magic('answer', 'Problem 3', '')
+
+
+# ---
+
+# In[10]:
+
+
+get_ipython().run_line_magic('problem', '4')
+
+
+# In[11]:
+
+
+get_ipython().run_cell_magic('answer', 'Problem 4', '')
+
+
+# ---
+
+# In[12]:
+
+
+get_ipython().run_line_magic('problem', '5')
+
+
+# In[13]:
+
+
+get_ipython().run_cell_magic('answer', 'Problem 5', '')
+
